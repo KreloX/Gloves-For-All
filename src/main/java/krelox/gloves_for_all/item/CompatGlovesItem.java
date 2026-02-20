9f2fa8010ab1ef2a4892271c63d03503fc975ee5
KreloX <krelox@seznam.cz>
2026-02-20 21:49:43 +0100

    refactor: simplify CompatGlovesItem implementation

--- src/main/java/krelox/gloves_for_all/item/CompatGlovesItem.java
+++ src/main/java/krelox/gloves_for_all/item/CompatGlovesItem.java
@@ -2,18 +2,14 @@ package krelox.gloves_for_all.item;
 
 import com.aetherteam.aether.item.accessories.gloves.GlovesItem;
 import krelox.gloves_for_all.GlovesForAll;
-import net.minecraft.world.item.ArmorMaterial;
-import net.minecraft.world.item.ItemStack;
-import top.theillusivec4.curios.api.SlotContext;
-import top.theillusivec4.curios.api.type.capability.ICurio;
 
 public class CompatGlovesItem extends GlovesItem {
     private final CompatArmorMaterial compatMaterial;
     private boolean isTrimmable = true;
 
     public CompatGlovesItem(CompatArmorMaterial material, double punchDamage, Properties properties) {
-        super(null, punchDamage, GlovesForAll.modLoc(material.getCompatModule().getSourceModId() + "/" + material.getSerializedName() + "_gloves"),
-                null, properties.durability(material.getUses()));
+        super(material.getArmorMaterial(), punchDamage, GlovesForAll.modLoc(material.getCompatModule().getSourceModId() + "/" + material.getSerializedName() + "_gloves"),
+                material.getArmorMaterial()::getEquipSound, properties.durability(material.getUses()));
         this.compatMaterial = material;
     }
 
@@ -28,26 +24,5 @@ public class CompatGlovesItem extends GlovesItem {
 
     public boolean isTrimmable() {
         return isTrimmable;
-    }
-
-    @Override
-    public ICurio.SoundInfo getEquipSound(SlotContext slotContext, ItemStack stack) {
-        return new ICurio.SoundInfo(getMaterial().getEquipSound(), 1.0F, 1.0F);
-    }
-
-    @SuppressWarnings("deprecation")
-    @Override
-    public int getEnchantmentValue() {
-        return getMaterial().getEnchantmentValue();
-    }
-
-    @Override
-    public boolean isValidRepairItem(ItemStack item, ItemStack material) {
-        return getMaterial().getRepairIngredient().test(material) || super.isValidRepairItem(item, material);
-    }
-
-    @Override
-    public ArmorMaterial getMaterial() {
-        return getCompatMaterial().getArmorMaterial();
     }
 }
