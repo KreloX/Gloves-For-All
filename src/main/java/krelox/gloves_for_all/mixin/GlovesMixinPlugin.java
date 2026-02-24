1d8288a054c8450506962ffc6ab4728beb21c985
KreloX <krelox@seznam.cz>
2026-02-24 10:08:28 +0100

    refactor: determine which mixins to apply by package names

--- src/main/java/krelox/gloves_for_all/mixin/GlovesMixinPlugin.java
+++ src/main/java/krelox/gloves_for_all/mixin/GlovesMixinPlugin.java
@@ -6,25 +6,9 @@ import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
-import static java.util.Map.entry;
-
 public class GlovesMixinPlugin implements IMixinConfigPlugin {
-    public final Map<String, String> mixinToModMap = Map.ofEntries(
-            entry("krelox.gloves_for_all.mixin.mixins.common.botania.ManasteelArmorItemMixin", "botania"),
-            entry("krelox.gloves_for_all.mixin.mixins.common.caverns_and_chasms.CCEventsMixin", "caverns_and_chasms"),
-            entry("krelox.gloves_for_all.mixin.mixins.common.caverns_and_chasms.EnchantmentHelperMixinMixin", "caverns_and_chasms"),
-            entry("krelox.gloves_for_all.mixin.mixins.common.cofh_core.ArmorItemCoFHMixin", "cofh_core"),
-            entry("krelox.gloves_for_all.mixin.mixins.common.create.CardboardArmorHandlerMixin", "create"),
-            entry("krelox.gloves_for_all.mixin.mixins.common.savage_and_ravage.BlastProofArmorTypeMixin", "savage_and_ravage"),
-            entry("krelox.gloves_for_all.mixin.mixins.common.savage_and_ravage.SREventsMixin", "savage_and_ravage"),
-            entry("krelox.gloves_for_all.mixin.mixins.common.thermal.ArmorEventsMixin", "thermal"),
-            entry("krelox.gloves_for_all.mixin.mixins.common.thermal.DivingArmorItemMixin", "thermal"),
-            entry("krelox.gloves_for_all.mixin.mixins.common.thermal_extra.CommonEventsMixin", "thermal_extra")
-    );
-
     @Override
     public void onLoad(String mixinPackage) {
     }
@@ -36,10 +20,12 @@ public class GlovesMixinPlugin implements IMixinConfigPlugin {
 
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-        if (mixinToModMap.containsKey(mixinClassName)) {
-            return LoadingModList.get().getModFileById(mixinToModMap.get(mixinClassName)) != null;
+        String[] path = mixinClassName.split("\\.");
+        String packageName = path[path.length - 2];
+        if (packageName.equals("accessor")) {
+            packageName = path[path.length - 3];
         }
-        return true;
+        return LoadingModList.get().getModFileById(packageName) != null;
     }
 
     @Override
