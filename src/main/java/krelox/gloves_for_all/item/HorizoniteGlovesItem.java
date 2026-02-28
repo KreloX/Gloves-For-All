3e2eb88102f004d4367f8e321ec71282d0c0a482
KreloX <krelox@seznam.cz>
2026-02-28 13:40:29 +0100

    fix: make horizonite gloves burn in furnaces

--- src/main/java/krelox/gloves_for_all/item/HorizoniteGlovesItem.java
+++ src/main/java/krelox/gloves_for_all/item/HorizoniteGlovesItem.java
@@ -4,6 +4,7 @@ import com.legacy.blue_skies.util.StringUtil;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
@@ -12,6 +13,11 @@ import java.util.List;
 public class HorizoniteGlovesItem extends BlueSkiesGlovesItem {
     public HorizoniteGlovesItem(CompatMaterial material, double punchDamage, Properties properties) {
         super(material, punchDamage, properties);
+    }
+
+    @Override
+    public int getBurnTime(ItemStack itemStack, @Nullable RecipeType<?> recipeType) {
+        return 1400 * 2;
     }
 
     @Override
